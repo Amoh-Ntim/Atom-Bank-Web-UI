2e75797e0781021a5257fbb57c2ad95edc5334d5
Amoh-Ntim <amohntim123@gmail.com>
2023-09-18 13:13:36 +0000

    Created the Atom Bank app with vite and tailwind

---
+++ AtomBank/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+// "build": "vite build",
+function App() {
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>Hello tailwind css with react</h1>
+    </>
+  )
+}
+
+export default App
