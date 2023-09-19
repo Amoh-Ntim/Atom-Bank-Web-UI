5b23f85280ac5ffd168e513fbb08eb9be77f42f0
Amoh-Ntim <amohntim123@gmail.com>
2023-09-19 19:05:46 +0000

    FInished Subscription

--- AtomBank/src/components/HeroSection Components/Subscribe.jsx
+++ AtomBank/src/components/HeroSection Components/Subscribe.jsx
@@ -1,7 +1,23 @@
 const Subscribe = () => {
   return (
     <div className="flex bg-gray-200 w-full items-center justify-center text-black-500 ">
-     <h3 className="text-5xl mt-12">Subsribe Newsletter</h3>
+      <div>
+        <div className="flex-col items-center">
+         <h3 className="text-5xl mt-12">Subsribe Newsletter</h3>
+         <h6 className="text-gray-500 mt-12">Financial transactions remotely using mobile</h6>
+        </div>
+
+        <div className=" justify-around mt-12 mb-12 sm: flex-col items-center md: flex-row">
+          <input
+            type="email"
+            className="px-4 py-2 mr-8 max-w-md border border-gray-300 rounded-l"
+            placeholder="Enter your email..."
+          />
+          <button className="px-4 py-2 bg-black text-white rounded-r">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
