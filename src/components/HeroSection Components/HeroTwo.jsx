c178a999cc136c8460d3664f44cdc474852ae984
Amoh-Ntim <amohntim123@gmail.com>
2023-09-20 22:53:09 +0000

    final commit

--- src/components/HeroSection Components/HeroTwo.jsx
+++ src/components/HeroSection Components/HeroTwo.jsx
@@ -3,7 +3,7 @@ const HeroTwo = () => {
   return (
     <div>
       <div className="flex mt-32 justify-around sm: flex-col items-center mt-48  md:flex-row">
-          <img className="hidden sm:flex" src="/image 18.png"/>
+          <img className="flex" src="/image 18.png"/>
           <div className="text-black mt-24 ">
               <h3 className="text-5xl mb-12" ><span className="font-bold">Atom</span>Bank For Your Easy</h3>
               <h6>The best place to transact and save money. More organized finances no </h6>
