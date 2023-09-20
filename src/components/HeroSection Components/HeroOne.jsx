9d2e4aabdc51fd29d7a85fd4136bbc7bacd46407
Amoh-Ntim <amohntim123@gmail.com>
2023-09-20 09:51:08 +0000

    Hidden phone image on small device

--- src/components/HeroSection Components/HeroOne.jsx
+++ src/components/HeroSection Components/HeroOne.jsx
@@ -24,12 +24,13 @@ const HeroOne = () => {
              {'->'}
            </p>
           </div>
+          {/* transition-transform transform scale-25 sm:scale-50 md:scale-75 lg:scale-100 */}
       </div>
       {/* Phone and Laptop images */}
       <div className="relative mt-16 mb-16 md:w-1/2 xs:justify-center">
       <img className="w-full h-auto" src="/image23.jpg" alt="My Image" />
-        <div className=" transition-transform transform scale-25 sm:scale-50 md:scale-75 lg:scale-100">
-        <img className=" absolute top-0 left-0 " src="/image16.jpg" alt="My Image" />
+        <div className=" opacity-0 md:opacity-100 lg:opacity-100">
+        <img className=" absolute top-0 left-0 ml-40" src="/image16.jpg" alt="My Image" />
         </div>
       </div>
     </div>
