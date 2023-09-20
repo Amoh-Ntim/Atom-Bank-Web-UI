c3bdc7acb2c979721da569c7f454a89247aae91e
Amoh-Ntim <amohntim123@gmail.com>
2023-09-20 09:23:10 +0000

    lap and phone image size

--- src/components/HeroSection Components/HeroOne.jsx
+++ src/components/HeroSection Components/HeroOne.jsx
@@ -26,9 +26,9 @@ const HeroOne = () => {
           </div>
       </div>
       {/* Phone and Laptop images */}
-      <div className="relative mt-16 mb-16 md:w-1/2 xs:justify-center max-w-full h-auto">
+      <div className="relative mt-16 mb-16 md:w-1/2 xs:justify-center">
       <img className="w-full h-auto" src="/image23.jpg" alt="My Image" />
-      <img className=" absolute top-0 left-0 transition-transform transform scale-25 sm:scale-50 md:scale-75 ml-40 lg:scale-100" src="/image16.jpg" alt="My Image" />
+      <img className=" absolute top-0 left-0  transition-transform transform scale-25 sm:scale-50 md:scale-75 ml-40 lg:scale-100" src="/image16.jpg" alt="My Image" />
       </div>
     </div>
       {/* Black Background */}
